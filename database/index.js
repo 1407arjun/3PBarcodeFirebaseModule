0d7341cb1f96ea6783696a45c7ab5395915581d1
Sharath Kumar Reddy Guvvala <guvvalasharath.kumar2020@vitstudent.ac.in>
2021-03-20 00:40:22 +0530

    Create index.js

---
+++ database/index.js
@@ -0,0 +1,12 @@
+//const express = require('express')
+const db = require('./mongoose')
+const barcodedata = require('./model/barcode')
+//const app = express()
+//const port = 8000
+/*app.listen(port,function(err){
+    if(err){
+        console.log('error in connecting to the server')
+        return
+    }
+    console.log('successfully connected to the server')
+})*/
