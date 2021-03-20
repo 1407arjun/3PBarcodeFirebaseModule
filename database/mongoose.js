83395591f88df190de4f51ce8b610417aaac66e6
Sharath Kumar Reddy Guvvala <guvvalasharath.kumar2020@vitstudent.ac.in>
2021-03-20 10:20:57 +0530

    Delete mongoose.js

--- database/mongoose.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/barcode_item_list')
-const db = mongoose.connection;
-db.on('error',console.log.bind(console,'error in connecting to db'))
-db.once('open',function(){
-    console.log('Successfully connected to the db')
-})
