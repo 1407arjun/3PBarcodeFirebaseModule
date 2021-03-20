81753f43be905b46a67f7d33b198fb8387a37a45
Sharath Kumar Reddy Guvvala <guvvalasharath.kumar2020@vitstudent.ac.in>
2021-03-20 10:21:27 +0530

    Delete barcode.js

--- database/model/barcode.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-const barcodeschema = new mongoose.Schema({
-
-})
-const barcodedata = mongoose.model('barcodedata',barcodeschema)
-module.exports = barcodedata
